c05add1a5d1a8318de13b1a8c49828fc49e15e05
Medveditskov Yuriy Sergeevich <sparin285@gmail.com>
2019-08-15 11:09:39 +0300

    Additional boilerplate

---
+++ Sparin.Documents/Services/Interfaces/ISchemaService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sparin.Documents.Services.Interfaces
+{
+    interface ISchemaService
+    {
+        
+    }
+}
---
+++ Sparin.Documents/Services/SchemaService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sparin.Documents.Services
+{
+    public class SchemaService
+    {
+    }
+}
